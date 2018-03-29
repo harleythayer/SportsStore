b699f85ed604017eefaa01c4605706dde7a44ecd
Harland Thayer <harley.thayer@gmail.com>
2018-03-29 15:08:27 -0400

    Initial commit

---
+++ src/main/java/com/hthayer/SportsStore/SportsStoreApplication.java
@@ -0,0 +1,12 @@
+package com.hthayer.SportsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsStoreApplication.class, args);
+	}
+}
